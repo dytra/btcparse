b5f245f1b88c24f85c75a266d2336813b71138b4
Dytra <widhptr40@gmail.com>
2016-11-03 11:09:23 +0700

    initialize files

---
+++ btcparse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3.4
+import requests
+import json
+from colorama import *
+
+BLUE  = Fore.BLUE+Style.BRIGHT
+GREEN = Fore.GREEN+Style.BRIGHT
+RESET = Style.RESET_ALL
+
+def info(str):
+	print("{}[-]{}{}".format(BLUE,RESET," " + str))
+
+def success(str):
+	print("{}[-]{}{}".format(GREEN,RESET," " + str))
+
+
+info("Parsing information from server...")
+url = "https://bitpay.com/api/rates"
+r	  = requests.get(url).text
+result = json.loads(r)
+
+parse = [i for i in result if i['code'] == "IDR"]
+rate  = int(parse[0]['rate'])
+code  = parse[0]['code']
+success("Information parsed successfully!")
+print("1 BTC = {} {}".format(rate,code))
+
