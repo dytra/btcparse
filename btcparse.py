97fe9e675c0a26c1eb3a6728ada6b8a69994e161
Dytra <widhptr40@gmail.com>
2016-11-04 10:08:14 +0700

    add user input

--- btcparse.py
+++ btcparse.py
@@ -1,26 +1,53 @@
 #!/usr/bin/python3.4
 import requests
 import json
+from sys import argv
 from colorama import *
 
 BLUE  = Fore.BLUE+Style.BRIGHT
 GREEN = Fore.GREEN+Style.BRIGHT
+RED   = Fore.RED+Style.BRIGHT
 RESET = Style.RESET_ALL
-
 def info(str):
 	print("{}[-]{}{}".format(BLUE,RESET," " + str))
 
 def success(str):
 	print("{}[+]{}{}".format(GREEN,RESET," " + str))
 
+def error(str):
+	print("{}[+]{}{}".format(RED,RESET," " + str))
 
+
+def usage():
+	print("Usage: ")
+	print("{} <currency>".format(argv[0]))
+	print("Ex: {} {}".format(argv[0],"usd"))
+
+def connect():
+	global result
+	url = "https://bitpay.com/api/rates"
+	try:
+		r	  = requests.get(url).text
+	except:
+		error("There was an error during connecting to the server")
+		exit()
+	result = json.loads(r)
+
+try:
+	inputuser = argv[1].upper() 
+except IndexError:
+	print("Syntax Error!")
+	usage()
+	exit()
 info("Parsing information from server...")
-url = "https://bitpay.com/api/rates"
-r	  = requests.get(url).text
-result = json.loads(r)
-
-parse = [i for i in result if i['code'] == "IDR"]
-rate  = int(parse[0]['rate'])
+connect()
+parse = [i for i in result if i['code'] == inputuser]
+try:
+	rate  = int(parse[0]['rate'])
+except:
+	error("ERROR: {} is not exist!".format(inputuser))
+	exit()
+	
 code  = parse[0]['code']
 success("Information parsed successfully!")
 print("1 BTC = {} {}".format(rate,code))
